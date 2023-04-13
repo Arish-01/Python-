3b23addce663409b7814deae12ab1661082c74ac
Arish <muraliarish@gmail.com>
2023-04-14 00:27:36 +0530

    Converting the data type of the variable

---
+++ Variables/typeconversion.py
@@ -0,0 +1,10 @@
+x,y,z=1,2.8,1j
+a=float(x) #here converting the x(int) into float using float keyword
+b=int(y) #float to int usinf int keyword
+c=complex(x) #int to complex using complex keyword
+print(a)
+print(b)
+print(c)
+print(type(a)) #using type keyword we can find the data type of the variable
+print(type(b))
+print(type(c))
