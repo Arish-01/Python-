207a4f780505f143630b05f80f4c594e8f7be59b
Arish <muraliarish@gmail.com>
2023-04-16 00:34:43 +0530

    looping and len() function

---
+++ Strings/loop&length.py
@@ -0,0 +1,8 @@
+a="Arish"
+for i in a: #Here i is refers to each character in a string thats prints in loop like sequence.
+    print(i)
+
+c=len(a) #len() finds the length of the string
+print(c)
+
+
