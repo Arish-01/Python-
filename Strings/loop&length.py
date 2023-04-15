b97e327949b8e94965ea18970b5541c486bb2086
Arish <muraliarish@gmail.com>
2023-04-16 00:39:21 +0530

    looping & len()

--- Strings/loop&length.py
+++ Strings/loop&length.py
@@ -5,4 +5,7 @@ for i in a: #Here i is refers to each character in a string thats prints in loop
 c=len(a) #len() finds the length of the string
 print(c)
 
-
+#By using len() printing the whole string
+for i in range(len(a)):
+    print(a[i])
+    i=i+1 #i+=1 also we can use
