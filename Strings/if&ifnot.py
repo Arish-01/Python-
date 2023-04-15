cb1956abbb35c1d1bf645ff400a800e3082277c6
Arish <muraliarish@gmail.com>
2023-04-16 00:48:58 +0530

    in & not in operators in string

---
+++ Strings/if&ifnot.py
@@ -0,0 +1,12 @@
+a="He is Python developer"
+ # by using in we check whether the certain character or phrase is present in a string.
+print("python" in a)
+ # by using in we check whether the certain character or phrase is not present in a string.
+print("python" not in a) 
+
+#using if statement we can do more like
+if("python" in a):
+    print("yes")
+#for not in 
+if("python" not in a):
+    print("no")
