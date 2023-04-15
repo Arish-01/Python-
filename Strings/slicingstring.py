50ea05746efd8bf2eca5fe9d63a66732f392a432
Arish <muraliarish@gmail.com>
2023-04-16 01:05:49 +0530

    Learned about slicing the string

---
+++ Strings/slicingstring.py
@@ -0,0 +1,7 @@
+a="he is a gentle man"
+#slicing the string from 15th postion in a string
+print(a[15:])
+#slicing from 15 to 17th postion 
+print(a[15:17])
+#slicing using the negative postion values 
+print(a[-10:-3])
